66992f97ec710606c7da46a3f0237a2fdc69f944
shivendra1718 <singh1718shivendra@gmail.com>
2026-02-12 14:20:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "api/") {
+    res.end("this is my home page");
+  } else if (req.url === "/about") {
+    res.end("this is my about page");
+  } else if (req.url === "/contactus") {
+    res.end("this is my contact us page");
+  } else if (req.url === "/home") {
+    res.end("this is my home page\n Welcome to ABES EC");
+  } else {
+    res.end("404 page not found");
+  }
+});
+
+server.listen(8000, () => {
+  console.log("Server is run");
+});
