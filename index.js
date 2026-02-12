1f93ab48f6acfda422d353e791308dc9b7d0cfad
shivendra1718 <singh1718shivendra@gmail.com>
2026-02-12 15:18:16 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "api/") {
+  if (req.url === "/") {
     res.end("this is my home page");
   } else if (req.url === "/about") {
-    res.end("this is my about page");
+    res.end(<img src="/image"></img>);
   } else if (req.url === "/contactus") {
     res.end("this is my contact us page");
   } else if (req.url === "/home") {
